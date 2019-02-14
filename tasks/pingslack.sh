6339eda6e0d0e23f66be8c302e0a865e423876a8
Sam Deere <dev@colophonemes.com>
2019-02-13 17:33:32 -0800

    Add browserless image, use to ping slack with report screenshot

--- tasks/pingslack.sh
+++ tasks/pingslack.sh
@@ -1,8 +1,36 @@
 #!/bin/sh
 
+read -d '' PUPPETEER_PAYLOAD << EOF
+{
+  "url": "$REPORT_URL",
+  "options": {
+    "fullPage": true,
+    "type": "png"
+  },
+  "gotoOptions": {
+    "waitUntil": "networkidle2"
+  }
+}
+EOF
+
+curl -s -X POST \
+  browserless:3000/screenshot \
+  -H 'Cache-Control: no-cache' \
+  -H 'Content-Type: application/json' \
+  -d "$PUPPETEER_PAYLOAD" > /tmp/amicrogenesis.png
+
+IMAGE_URL=$(curl --upload-file /tmp/amicrogenesis.png https://transfer.sh/amicrogenesis.png)
+
 read -d '' JSON_PAYLOAD << EOF
 {
-  "text": "$REPORT_URL"
+  "attachments": [
+    {
+      "title": "Amicrogenesis Progress",
+      "title_link": "$REPORT_URL",
+      "fallback": "$REPORT_URL",
+      "image_url": "$IMAGE_URL"
+    }
+  ]
 }
 EOF
 
